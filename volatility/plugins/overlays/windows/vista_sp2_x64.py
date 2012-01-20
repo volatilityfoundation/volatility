2025ef121dbfa74f96a8b79d23d25a6580611f0f
mike.auty@gmail.com <mike.auty@gmail.com>
2012-01-20 21:03:36 +0000

    Add in windows 64-bit profiles to trunk!
    It's been a long time coming, but it's finally here!
    YAY!  5:)

---
+++ volatility/plugins/overlays/windows/vista_sp2_x64.py
@@ -0,0 +1,63 @@
+# Volatility
+# Copyright (c) 2008 Volatile Systems
+# Copyright (c) 2008-2011 Volatile Systems
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details. 
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+#
+
+"""
+@author:       Jamie Levy (Gleeda)
+@license:      GNU General Public License 2.0 or later
+@contact:      jamie.levy@gmail.com
+
+This file provides support for Windows Vista SP 2. We provide a profile
+for SP2.
+"""
+
+#pylint: disable-msg=C0111
+
+import copy
+import vista_sp1_x64
+import vista_sp2_x64_vtypes
+import vista_sp12_x86_syscalls
+import windows64
+# import tcpip_vtypes
+import crash_vtypes
+import hibernate_vtypes
+import kdbg_vtypes
+import volatility.debug as debug #pylint: disable-msg=W0611
+
+overlay = copy.deepcopy(vista_sp1_x64.overlay)
+
+object_classes = copy.deepcopy(vista_sp1_x64.object_classes)
+
+vtypes = copy.deepcopy(vista_sp2_x64_vtypes.ntkrnlmp_types)
+
+vtypes.update(crash_vtypes.crash_vtypes)
+vtypes.update(hibernate_vtypes.hibernate_vtypes)
+vtypes.update(kdbg_vtypes.kdbg_vtypes)
+#vtypes.update(tcpip_vtypes.tcpip_vtypes_vista)
+
+class VistaSP2x64(windows64.AbstractWindowsX64):
+    """ A Profile for Windows Vista SP2 x64 """
+    _md_major = 6
+    _md_minor = 0
+    overlay = overlay
+    abstract_types = vtypes
+    object_classes = object_classes
+    syscalls = vista_sp12_x86_syscalls.syscalls
+
+class Win2K8SP2x64(VistaSP2x64):
+    """ A Profile for Windows 2008 SP2 x64 """
---
+++ volatility/plugins/overlays/windows/win7_sp1_x64.py
@@ -0,0 +1,63 @@
+# Volatility
+# Copyright (c) 2008 Volatile Systems
+# Copyright (c) 2008-2011 Volatile Systems
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details. 
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+#
+
+"""
+@author:       Michael Cohen
+@license:      GNU General Public License 2.0 or later
+@contact:      scudette@gmail.com
+
+This file provides support for Windows 7 SP 1 64 bit version. Many thanks to
+Alex Pease (alex.pease@gmail.com) for his assistance.
+"""
+
+#pylint: disable-msg=C0111
+
+import copy
+import win7_sp0_x64
+import win7_sp1_x64_vtypes
+import win7_sp0_x86_syscalls
+import windows64
+import tcpip_vtypes
+import crash_vtypes
+import hibernate_vtypes
+import kdbg_vtypes
+import volatility.debug as debug #pylint: disable-msg=W0611
+
+overlay = copy.deepcopy(win7_sp0_x64.overlay)
+
+object_classes = copy.deepcopy(win7_sp0_x64.object_classes)
+
+vtypes = copy.deepcopy(win7_sp1_x64_vtypes.ntkrnlmp_types)
+
+vtypes.update(crash_vtypes.crash_vtypes)
+vtypes.update(hibernate_vtypes.hibernate_vtypes)
+# vtypes.update(hibernate_vtypes.hibernate_win7_vtypes)
+#vtypes.update(hibernate_vtypes.hibernate_win7_x64_vtypes)
+vtypes.update(kdbg_vtypes.kdbg_vtypes)
+vtypes.update(tcpip_vtypes.tcpip_vtypes_vista)
+vtypes.update(tcpip_vtypes.tcpip_vtypes_7)
+
+class Win7SP1x64(windows64.AbstractWindowsX64):
+    """ A Profile for Windows 7 SP1 x64 """
+    _md_major = 6
+    _md_minor = 1
+    overlay = overlay
+    abstract_types = vtypes
+    object_classes = object_classes
+    syscalls = win7_sp0_x86_syscalls.syscalls
