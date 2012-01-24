a08fd0df3c2db3c6efa3f9568a04ccb3a228fb86
iMHLv2 <michael.ligh@mnin.org>
2012-01-24 07:07:56 +0000

    add some comments and a few minor changes to ssdt_vtypes

--- volatility/plugins/overlays/windows/ssdt_vtypes.py
+++ volatility/plugins/overlays/windows/ssdt_vtypes.py
@@ -1,3 +1,4 @@
+# SSDT structures for all x86 profiles *except* Win 2003 Server
 ssdt_vtypes = {
     '_SERVICE_DESCRIPTOR_TABLE' : [ 0x40, {
     'Descriptors' : [0x0, ['array', 4, ['_SERVICE_DESCRIPTOR_ENTRY']]],
@@ -5,13 +6,14 @@ ssdt_vtypes = {
     '_SERVICE_DESCRIPTOR_ENTRY' : [ 0x10, {
     'KiServiceTable' : [0x0, ['pointer', ['void']]],
     'CounterBaseTable' : [0x4, ['pointer', ['unsigned long']]],
-    'ServiceLimit' : [0x8, ['long']],
+    'ServiceLimit' : [0x8, ['unsigned long']],
     'ArgumentTable' : [0xc, ['pointer', ['unsigned char']]],
     }],
 }
 
+# SSDT structures for Win 2003 Server x86
 ssdt_vtypes_2k3 = {
-  '_SERVICE_DESCRIPTOR_TABLE' : [ 0x40, {
+    '_SERVICE_DESCRIPTOR_TABLE' : [ 0x20, {
     'Descriptors' : [0x0, ['array', 2, ['_SERVICE_DESCRIPTOR_ENTRY']]],
     }],
 }
