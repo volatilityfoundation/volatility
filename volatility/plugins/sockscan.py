bbca57c482273fdccf39020d9e8a549ec047ed03
iMHLv2 <michael.ligh@mnin.org>
2012-02-01 23:38:30 +0000

    remove my sloppy print statement that snuck in with r1331

--- volatility/plugins/sockscan.py
+++ volatility/plugins/sockscan.py
@@ -44,7 +44,6 @@ class CheckSocketCreateTime(scan.ScannerCheck):
 
     def check(self, offset):
         start_of_object = self.address_space.profile.get_obj_offset("_POOL_HEADER", "PoolTag")
-        print hex(start_of_object)
         address_obj = obj.Object('_ADDRESS_OBJECT', vm = self.address_space,
                                 offset = offset + start_of_object)
 
