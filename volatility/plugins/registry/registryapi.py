82ca06731abe40f11a96c29788562ab3c6cc92f5
mike.auty@gmail.com <mike.auty@gmail.com>
2012-05-06 23:16:26 +0000

    Add in Gleeda's registryapi module and shimcache plugin.

---
+++ volatility/plugins/registry/registryapi.py
@@ -0,0 +1,271 @@
+# Volatility
+# Copyright (C) 2008-2010 Volatile Systems
+# Copyright (C) 2011 Jamie Levy (Gleeda) <jamie.levy@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details. 
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+#
+
+"""
+@author:       Jamie Levy (Gleeda)
+@license:      GNU General Public License 2.0 or later
+@contact:      jamie.levy@gmail.com
+@organization: Volatile Systems
+"""
+
+import volatility.win32.hive as hivemod
+import volatility.win32.rawreg as rawreg
+import volatility.win32.hashdump as hashdump
+import volatility.utils as utils
+import volatility.plugins.registry.hivelist as hl
+from heapq import nlargest
+
+
+class RegistryApi(object):
+    """A wrapper several highly used Registry functions"""
+
+    def __init__(self, config):
+        self._config = config
+        self.addr_space = utils.load_as(self._config)
+        self.all_offsets = {}
+        self.current_offsets = {}
+        self.populate_offsets()
+
+    def print_offsets(self):
+        '''
+        this is just in case we want to check our offsets and which hive(s) was/were chosen
+        '''
+        for item in self.all_offsets:
+            print item, self.all_offsets[item]
+        for item in self.current_offsets:
+            print 'current', item, self.current_offsets[item]
+
+    def populate_offsets(self):
+        '''
+        get all hive offsets so we don't have to scan again...
+        '''
+        hive_offsets = []
+        hiveroot = hl.HiveList(self._config).calculate()
+
+        for hive in hiveroot:
+            if hive.obj_offset not in hive_offsets:
+                hive_offsets.append(hive.obj_offset)
+                try:
+                    name = hive.FileFullPath.v() or hive.FileUserName.v() or hive.HiveRootPath.v() or "[no name]"
+                # What exception are we expecting here?
+                except:
+                    name = "[no name]"
+                self.all_offsets[hive.obj_offset] = name
+
+    def reg_get_currentcontrolset(self, fullname = True):
+        '''
+        get the CurrentControlSet
+            If fullname is not specified, we only get the number like "1" or "2" etc
+            The default is ControlSet00{#} so we can append it to the desired key path
+            We return None if it fails, so you need to verify before using.
+        '''
+        for offset in self.all_offsets:
+            name = self.all_offsets[offset] + " "
+            if name.lower().find("\\system ") != -1:
+                sysaddr = hivemod.HiveAddressSpace(self.addr_space, self._config, offset)
+                if fullname:
+                    return "ControlSet00{0}".format(hashdump.find_control_set(sysaddr))
+                else:
+                    return hashdump.find_control_set(sysaddr)
+        return None
+
+    def set_current(self, hive_name = None, user = None):
+        '''
+        if we find a hive that fits the given criteria, save its offset 
+        so we don't have to scan again.  this can be reset using reset_current
+        if context changes
+        '''
+        for item in self.all_offsets:
+            name = self.all_offsets[item] + " "
+            if user == None and hive_name == None:
+                #no particular preference: all hives
+                self.current_offsets[item] = name
+            elif user != None and name.lower().find('\\' + user.lower() + '\\') != -1 and name.lower().find("\\" + "ntuser.dat ") != -1:
+                #user's NTUSER.DAT hive
+                self.current_offsets[item] = name
+            elif hive_name != None and hive_name.lower() == 'hklm' \
+                and (name.lower().find("\\security ") != -1 or name.lower().find("\\system ") != -1 \
+                or name.lower().find("\\software ") != -1 or name.lower().find("\\sam ") != -1):
+                #any HKLM hive 
+                self.current_offsets[item] = name
+            elif hive_name != None and name.lower().find("\\" + hive_name.lower() + " ") != -1 and user == None:
+                #a particular hive indicated by hive_name
+                if hive_name.lower() == "system" and name.lower().find("\\syscache.hve ") == -1:
+                    self.current_offsets[item] = name
+                elif hive_name.lower() != "system":
+                    self.current_offsets[item] = name
+
+    def reset_current(self):
+        '''
+        this is in case we switch to a different hive/user/context
+        '''
+        self.current_offsets = {}
+
+    def reg_get_key(self, hive_name, key, user = None, given_root = None):
+        '''
+        Returns a key from a requested hive; assumes this is from a single hive
+        if more than one hive is specified, the hive/key found is returned
+        '''
+        if self.all_offsets == {}:
+            self.populate_offsets()
+        if self.current_offsets == {}:
+            self.set_current(hive_name, user)
+        if key:
+            for offset in self.current_offsets:
+                if given_root == None:
+                    h = hivemod.HiveAddressSpace(self.addr_space, self._config, offset)
+                    root = rawreg.get_root(h)
+                else:
+                    root = given_root
+                if root != None:
+                    k = rawreg.open_key(root, key.split('\\'))
+                    if k:
+                        return k
+        return None
+
+    def reg_yield_key(self, hive_name, key, user = None, given_root = None):
+        ''' 
+        Use this function if you are collecting keys from more than one hive
+        '''
+        if self.all_offsets == {}:
+            self.populate_offsets()
+        if self.current_offsets == {}:
+            self.set_current(hive_name, user)
+        if key:
+            for offset in self.current_offsets:
+                name = self.current_offsets[offset]
+                if given_root == None:
+                    h = hivemod.HiveAddressSpace(self.addr_space, self._config, offset)
+                    root = rawreg.get_root(h)
+                else:
+                    root = given_root
+                if root != None:
+                    k = rawreg.open_key(root, key.split('\\'))
+                    if k:
+                        yield k, name
+
+    def reg_enum_key(self, hive_name, key, user = None):
+        '''
+        This function enumerates the requested key
+        '''
+        k = self.reg_get_key(hive_name, key, user)
+        if k:
+            for s in rawreg.subkeys(k):
+                if s.Name:
+                    item = key + '\\' + s.Name
+                    yield item
+
+    def reg_get_all_subkeys(self, hive_name, key, user = None, given_root = None):
+        '''
+        This function enumerates the subkeys of the requested key
+        '''
+        if given_root == None:
+            k = self.reg_get_key(hive_name, key, user)
+        else:
+            k = given_root
+        if k:
+            for s in rawreg.subkeys(k):
+                if s.Name:
+                    yield s
+
+    def reg_get_value(self, hive_name, key, value, strcmp = None):
+        '''
+        This function returns the requested value of a registry key
+        '''
+        if key and value:
+            h = self.reg_get_key(hive_name, key)
+            if h != None:
+                for v in rawreg.values(h):
+                    if value == v.Name:
+                        tp, dat = rawreg.value_data(v)
+                        if tp == 'REG_BINARY' or strcmp == None:
+                            # We want raw data
+                            return dat
+                        else:
+                            # This is a string comparison
+                            dat = str(dat)
+                            dat = dat.strip()
+                            dat = ''.join([x for x in dat if ord(x) != 0])  #get rid of funky nulls for string comparison
+                            if strcmp == dat:
+                                return dat
+        return None
+
+    def reg_get_all_keys(self, hive_name, user = None, start = None, end = None, reg = False):
+        '''
+        This function enumerates all keys in specified hives and 
+        collects lastwrite times.
+        '''
+        keys = []
+        if self.all_offsets == {}:
+            self.populate_offsets()
+        if self.current_offsets == {}:
+            self.set_current(hive_name, user)
+
+        # Collect the root keys 
+        for offset in self.current_offsets:
+            reg_name = self.current_offsets[offset]
+            h = hivemod.HiveAddressSpace(self.addr_space, self._config, offset)
+            root = rawreg.get_root(h)
+            if not root:
+                pass
+            else:
+                time = "{0}".format(root.LastWriteTime)
+                if reg:
+                    yield (time, reg_name, root.Name)
+                else:
+                    yield (time, root.Name)
+                for s in rawreg.subkeys(root):
+                    if reg:
+                        keys.append([s, reg_name, root.Name + "\\" + s.Name])
+                    else:
+                        keys.append([s, root.Name + "\\" + s.Name])
+
+        # Get subkeys
+        for k, reg_name, name in keys:
+            time = "{0}".format(k.LastWriteTime)
+            if start and end and time >= start and time <= end:
+                if reg:
+                    yield (time, reg_name, name)
+                else:
+                    yield (time, name)
+            elif start == None and end == None:
+                if reg:
+                    yield (time, reg_name, name)
+                else:
+                    yield (time, name)
+
+            for s in rawreg.subkeys(k):
+                if name and s.Name:
+                    item = name + '\\' + s.Name
+                    if reg:
+                        keys.append([s, reg_name, item])
+                    else:
+                        keys.append([s, item])
+
+
+    def reg_get_last_modified(self, hive_name, count = 1, user = None, start = None, end = None, reg = False):
+        '''
+        Wrapper function using reg_get_all_keys. These functions can take a WHILE since all 
+        subkeys have to be collected before you can compare lastwrite times.
+        '''
+        data = nlargest(count, self.reg_get_all_keys(hive_name, user, start, end, reg))
+        for t, _, name in data:
+            yield (t, name)
+
+
---
+++ volatility/plugins/registry/shimcache.py
@@ -0,0 +1,261 @@
+# Volatility
+# Copyright (C) 2008-2012 Volatile Systems
+# Copyright (C) 2011 Jamie Levy (Gleeda) <jamie.levy@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details. 
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+#
+
+"""
+@author:       Jamie Levy (gleeda)
+@license:      GNU General Public License 2.0 or later
+@contact:      jamie.levy@gmail.com
+@organization: Volatile Systems
+"""
+
+import volatility.plugins.registry.registryapi as registryapi
+import volatility.debug as debug
+import volatility.utils as utils
+import volatility.obj as obj
+import volatility.commands as commands
+import volatility.addrspace as addrspace
+
+# Structures taken from the ShimCache Whitepaper: https://blog.mandiant.com/archives/2459
+
+#### SHIMRECS ####
+
+shimrecs_type_xp = {
+    'ShimRecords' : [ None, {
+        'Magic' : [ 0x0, ['unsigned int']], #0xDEADBEEF
+        'NumRecords' : [ 0x8, ['short']],
+        'Entries' : [0x190, ['array', lambda x: x.NumRecords, ['AppCompatCacheEntry']]],
+    } ],
+}
+
+shimrecs_type_2003vista = {
+    'ShimRecords' : [ None, {
+        'Magic' : [ 0x0, ['unsigned int']], #0xBADC0FFE
+        'NumRecords' : [ 0x4, ['int']],
+        'Entries' : [0x8, ['array', lambda x: x.NumRecords, ['AppCompatCacheEntry']]],
+    } ],
+}
+
+shimrecs_type_win7 = {
+    'ShimRecords' : [ None, {
+        'Magic' : [ 0x0, ['unsigned int']], #0xBADC0FFE
+        'NumRecords' : [ 0x4, ['int']],
+        'Entries' : [0x80, ['array', lambda x: x.NumRecords, ['AppCompatCacheEntry']]],
+    } ],
+}
+
+#### APPCOMPAT TYPES ####
+
+appcompat_type_xp_x86 = {
+    'AppCompatCacheEntry' : [ 0x228, {
+        'Path' : [ 0x0, ['array', 0x208, ['char']]],
+        'LastModified' : [ 0x210, ['WinTimeStamp', {}]],
+        'FileSize': [0x218, ['long long']],
+        'LastUpdate' : [ 0x220, ['WinTimeStamp', {}]],
+    } ],
+}
+
+appcompat_type_2003_x86 = {
+    'AppCompatCacheEntry' : [ 0x18, {
+        'Length' : [ 0x0, ['unsigned short']],
+        'MaximumLength' : [0x2, ['unsigned short']],
+        'PathOffset' : [ 0x4, ['unsigned int']],
+        'LastModified' : [ 0x8, ['WinTimeStamp', {}]],
+        'FileSize': [0x10, ['_LARGE_INTEGER']],
+    } ],
+}
+
+appcompat_type_vista_x86 = {
+    'AppCompatCacheEntry' : [ 0x18, {
+        'Length' : [ 0x0, ['unsigned short']],
+        'MaximumLength' : [0x2, ['unsigned short']],
+        'PathOffset' : [ 0x4, ['unsigned int']],
+        'LastModified' : [ 0x8, ['WinTimeStamp', {}]],
+        'InsertFlags' : [0x10, ['unsigned int']],
+        'Flags' : [0x14, ['unsigned int']],
+    } ],
+}
+
+appcompat_type_win7_x86 = {
+    'AppCompatCacheEntry' : [ 0x20, {
+        'Length' : [ 0x0, ['unsigned short']],
+        'MaximumLength' : [0x2, ['unsigned short']],
+        'PathOffset' : [ 0x4, ['unsigned int']],
+        'LastModified' : [ 0x8, ['WinTimeStamp', {}]],
+        'InsertFlags' : [0x10, ['unsigned int']],
+        'ShimFlags' : [0x14, ['unsigned int']],
+        'BlobSize' : [0x18, ['unsigned int']],
+        'BlobOffset' : [0x1c, ['unsigned int']],
+    } ],
+}
+
+appcompat_type_2003_x64 = {
+    'AppCompatCacheEntry' : [ 0x20, {
+        'Length' : [ 0x0, ['unsigned short']],
+        'MaximumLength' : [0x2, ['unsigned short']],
+        'PathOffset' : [ 0x8, ['unsigned long long']],
+        'LastModified' : [ 0x10, ['WinTimeStamp', {}]],
+        'FileSize': [0x18, ['_LARGE_INTEGER']],
+    } ],
+}
+
+appcompat_type_vista_x64 = {
+    'AppCompatCacheEntry' : [ 0x20, {
+        'Length' : [ 0x0, ['unsigned short']],
+        'MaximumLength' : [0x2, ['unsigned short']],
+        'PathOffset' : [ 0x8, ['unsigned int']],
+        'LastModified' : [ 0x10, ['WinTimeStamp', {}]],
+        'InsertFlags' : [0x18, ['unsigned int']],
+        'Flags' : [0x1c, ['unsigned int']],
+    } ],
+}
+
+appcompat_type_win7_x64 = {
+    'AppCompatCacheEntry' : [ 0x30, {
+        'Length' : [ 0x0, ['unsigned short']],
+        'MaximumLength' : [0x2, ['unsigned short']],
+        'PathOffset' : [ 0x8, ['unsigned long long']],
+        'LastModified' : [ 0x10, ['WinTimeStamp', {}]],
+        'InsertFlags' : [0x18, ['unsigned int']],
+        'ShimFlags' : [0x1c, ['unsigned int']],
+        'BlobSize' : [0x20, ['unsigned long long']],
+        'BlobOffset' : [0x28, ['unsigned long long']],
+    } ],
+}
+
+class ShimCacheTypesXPx86(obj.ProfileModification):
+    before = ['WindowsObjectClasses']
+    conditions = {'os': lambda x: x == 'windows',
+                  'major': lambda x: x == 5,
+                  'minor': lambda x: x == 1,
+                  'memory_model': lambda x: x == '32bit'}
+    def modification(self, profile):
+        profile.vtypes.update(shimrecs_type_xp)
+        profile.vtypes.update(appcompat_type_xp_x86)
+
+
+class ShimCacheTypes2003x86(obj.ProfileModification):
+    before = ['WindowsObjectClasses']
+    conditions = {'os': lambda x: x == 'windows',
+                  'major': lambda x: x == 5,
+                  'minor': lambda x: x == 2,
+                  'memory_model': lambda x: x == '32bit'}
+    def modification(self, profile):
+        profile.vtypes.update(shimrecs_type_2003vista)
+        profile.vtypes.update(appcompat_type_2003_x86)
+
+class ShimCacheTypesVistax86(obj.ProfileModification):
+    before = ['WindowsObjectClasses']
+    conditions = {'os': lambda x: x == 'windows',
+                  'major': lambda x: x == 6,
+                  'minor': lambda x: x == 0,
+                  'memory_model': lambda x: x == '32bit'}
+    def modification(self, profile):
+        profile.vtypes.update(shimrecs_type_2003vista)
+        profile.vtypes.update(appcompat_type_vista_x86)
+
+class ShimCacheTypesWin7x86(obj.ProfileModification):
+    before = ['WindowsObjectClasses']
+    conditions = {'os': lambda x: x == 'windows',
+                  'major': lambda x: x == 6,
+                  'minor': lambda x: x == 1,
+                  'memory_model': lambda x: x == '32bit'}
+    def modification(self, profile):
+        profile.vtypes.update(shimrecs_type_win7)
+        profile.vtypes.update(appcompat_type_win7_x86)
+
+class ShimCacheTypes2003x64(obj.ProfileModification):
+    before = ['WindowsObjectClasses']
+    conditions = {'os': lambda x: x == 'windows',
+                  'major': lambda x: x == 5,
+                  'minor': lambda x: x == 2,
+                  'memory_model': lambda x: x == '64bit'}
+    def modification(self, profile):
+        profile.vtypes.update(shimrecs_type_2003vista)
+        profile.vtypes.update(appcompat_type_2003_x64)
+
+class ShimCacheTypesVistax64(obj.ProfileModification):
+    before = ['WindowsObjectClasses']
+    conditions = {'os': lambda x: x == 'windows',
+                  'major': lambda x: x == 6,
+                  'minor': lambda x: x == 0,
+                  'memory_model': lambda x: x == '64bit'}
+    def modification(self, profile):
+        profile.vtypes.update(shimrecs_type_2003vista)
+        profile.vtypes.update(appcompat_type_vista_x64)
+
+class ShimCacheTypesWin7x64(obj.ProfileModification):
+    before = ['WindowsObjectClasses']
+    conditions = {'os': lambda x: x == 'windows',
+                  'major': lambda x: x == 6,
+                  'minor': lambda x: x == 1,
+                  'memory_model': lambda x: x == '64bit'}
+    def modification(self, profile):
+        profile.vtypes.update(shimrecs_type_win7)
+        profile.vtypes.update(appcompat_type_win7_x64)
+
+
+class ShimCache(commands.Command):
+
+    @staticmethod
+    def is_valid_profile(profile):
+        return profile.metadata.get('os', 'unknown').lower() == 'windows'
+
+    def remove_unprintable(self, item):
+        return ''.join([str(c) for c in item if (ord(c) > 31 or ord(c) == 9) and ord(c) <= 126])
+
+    def calculate(self):
+        addr_space = utils.load_as(self._config)
+        regapi = registryapi.RegistryApi(self._config)
+        regapi.reset_current()
+        currentcs = regapi.reg_get_currentcontrolset()
+        if currentcs == None:
+            currentcs = "ControlSet001"
+
+        version = (addr_space.profile.metadata.get('major', 0),
+                   addr_space.profile.metadata.get('minor', 0))
+        xp = False
+
+        if version <= (5, 1):
+            key = currentcs + '\\' + "Control\\Session Manager\\AppCompatibility"
+            xp = True
+        else:
+            key = currentcs + '\\' + "Control\\Session Manager\\AppCompatCache"
+
+        data_raw = regapi.reg_get_value('system', key, "AppCompatCache")
+        if data_raw == None or len(data_raw) < 0x1c:
+            debug.error("No ShimCache data found")
+
+        bufferas = addrspace.BufferAddressSpace(self._config, data = data_raw)
+        shimdata = obj.Object("ShimRecords", offset = 0, vm = bufferas)
+        if shimdata == None:
+            debug.error("No ShimCache data found")
+
+        for e in shimdata.Entries:
+            if xp:
+                path = str(''.join([str(c) for c in e.Path]))
+                yield self.remove_unprintable(path), e.LastModified, e.LastUpdate
+            else:
+                yield self.remove_unprintable(bufferas.read(int(e.PathOffset), int(e.Length))), e.LastModified, None
+
+    def render_text(self, outfd, data):
+        for path, lm, lu in data:
+            if lu:
+                outfd.write("Last Modified: {0}, Lastupdate: {1}, Path: {2}\n".format(lm, lu, path))
+            else:
+                outfd.write("Last Modified: {0}, Path: {1}\n".format(lm, path))
