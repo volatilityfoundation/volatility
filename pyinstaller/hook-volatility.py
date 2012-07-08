1bbff68e01ff16eb0399b5a72106a194e75f7bd9
mike.auty@gmail.com <mike.auty@gmail.com>
2012-07-08 18:06:25 +0000

    Pylint the pyinstaller spec file.

--- pyinstaller/hook-volatility.py
+++ pyinstaller/hook-volatility.py
@@ -1,6 +1,5 @@
 
 import os
-import sys
 
 projpath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -12,14 +11,14 @@ for dirpath, _dirnames, filenames in os.walk(os.path.join(projpath, 'volatility'
         dirpath = dirpath[1:]
     for filename in filenames:
         path = os.path.join(dirpath, os.path.splitext(filename)[0])
-	if "/." in path:
-	    continue
-	if "__" in path:
-	    continue
+        if "/." in path:
+            continue
+        if "__" in path:
+            continue
 
-	path = path.replace("-", "_")
-	path = path.replace(os.path.sep, ".")
+        path = path.replace("-", "_")
+        path = path.replace(os.path.sep, ".")
 
-	modules.add("volatility.plugins." + path)
+        modules.add("volatility.plugins." + path)
 
 hiddenimports = list(modules)
