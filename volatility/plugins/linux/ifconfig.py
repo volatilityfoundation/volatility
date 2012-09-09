3f36d407b1bee10504d34646cbfa5e756ceb2635
atcuno <andrew@memoryanalysis.net>
2012-09-09 04:07:35 +0000

    Linux - update ifconfig to report if the interface is in promiscuos mode

--- volatility/plugins/linux/ifconfig.py
+++ volatility/plugins/linux/ifconfig.py
@@ -73,7 +73,8 @@ class linux_ifconfig(linux_common.AbstractLinuxCommand):
 
         self.table_header(outfd, [("Interface", "16"), 
                                   ("IP Address", "20"), 
-                                  ("MAC Address", "")])
+                                  ("MAC Address", "18"),
+                                  ("Promiscous Modue", "5")])
                                   
         for net_dev, in_dev in data:
 
@@ -90,5 +91,7 @@ class linux_ifconfig(linux_common.AbstractLinuxCommand):
 
             mac_addr = ":".join(["{0:02x}".format(x) for x in hwaddr][:6])
 
-            self.table_row(outfd, net_dev.name, ip, mac_addr)
+            promisc = ["False", "True"][(net_dev.flags & 0x100) == 0x100] # IFF_PROMISC
+
+            self.table_row(outfd, net_dev.name, ip, mac_addr, promisc)
 
