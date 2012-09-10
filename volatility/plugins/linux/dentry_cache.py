eececca15683a47562e94d92d943080862bdb8dc
atcuno <andrew@memoryanalysis.net>
2012-09-10 04:40:57 +0000

    Linux - add dentry cache

---
+++ volatility/plugins/linux/dentry_cache.py
@@ -0,0 +1,86 @@
+# Volatility
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details. 
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+
+"""
+@author:       Andrew Case
+@license:      GNU General Public License 2.0 or later
+@contact:      atcuno@gmail.com
+@organization:
+"""
+
+import volatility.obj as obj
+import volatility.plugins.linux.common as linux_common
+from volatility.plugins.linux.slab_info import linux_slabinfo
+
+class linux_dentry_cache(linux_common.AbstractLinuxCommand):
+    """Gather files from the dentry cache"""
+    
+    def __init__(self, config, *args): 
+        linux_common.AbstractLinuxCommand.__init__(self, config, *args)
+        self._config.add_option('UNALLOCATED', short_option = 'u', 
+                        default = False,
+                        help = 'Show unallocated',
+                        action = 'store_true')       
+    
+    # we can't get the full path b/c we 
+    # do not have a ref to the vfsmnt
+    def get_partial_path(self, dentry):
+        path = []
+
+        name = ""
+
+        while dentry and dentry != dentry.d_parent:
+            name = dentry.d_name.name.dereference_as("String", length=255)
+            if name.is_valid():
+                path.append(str(name))
+            dentry = dentry.d_parent
+
+        path.reverse()
+            
+        str_path = "/".join([p for p in path])        
+        
+        return str_path
+
+    def make_body(self, path, dentry):
+        i = dentry.d_inode
+
+        # MD5|name|inode|mode_as_string|UID|GID|size|atime|mtime|ctime|crtime
+        if i:
+            ret = [0, path, i.i_ino, 0, i.i_uid, i.i_gid, i.i_size, i.i_atime, i.i_mtime, 0, i.i_ctime]             
+        else:
+            ret = [0, path] + [0] * 8
+
+        ret = "|".join([str(val) for val in ret]) 
+        return ret
+
+    def calculate(self):
+        linux_common.set_plugin_members(self)        
+            
+        cache = linux_slabinfo(self._config).get_kmem_cache("dentry", self._config.UNALLOCATED)
+            
+        for dentry in cache:
+            path     = self.get_partial_path(dentry)
+            bodyline = self.make_body(path, dentry)
+
+            yield bodyline
+
+    def render_text(self, outfd, data):
+
+        for bodyline in data:
+            outfd.write(bodyline + "\n")
+
+
+ 
