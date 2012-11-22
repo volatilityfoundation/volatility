9d881ed2b04867d0dae2832ff28ae286da82b881
iMHLv2 <michael.ligh@mnin.org>
2012-11-22 02:55:52 +0000

    a bug in the idt plugin prevented it from displaying the right interrupt handler address when special gdt selectors are set

---
+++ volatility/plugins/addrspaces/hpak.py
@@ -0,0 +1,114 @@
+# Volatility
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details. 
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+#
+
+import zlib
+import volatility.obj as obj
+import volatility.plugins.addrspaces.standard as standard
+
+class HPAKVTypes(obj.ProfileModification):
+    def modification(self, profile):
+        profile.vtypes.update({
+            'HPAK_HEADER' : [ 0x20, { 
+                'Magic' : [ 0, ['String', dict(length = 4)]], 
+                }], 
+            'HPAK_SECTION': [ 0xE0, { 
+                'Header' : [ 0, ['String', dict(length = 32)]], 
+                'Compressed' : [ 0x8C, ['unsigned int']], 
+                'Length' : [ 0x98, ['unsigned long long']], 
+                'Offset' : [ 0xA8, ['unsigned long long']], 
+                'NextSection' : [ 0xB0, ['unsigned long long']], 
+                'Name' : [ 0xD4, ['String', dict(length = 12)]], 
+                }], 
+            })
+        profile.object_classes.update({'HPAK_HEADER': HPAK_HEADER})
+            
+class HPAK_HEADER(obj.CType):
+    """A class for B.S. Hairy headers"""
+    
+    def Sections(self):
+    
+        ## The initial section object 
+        section = obj.Object("HPAK_SECTION", 
+                             offset = self.obj_vm.profile.get_obj_size("HPAK_HEADER"), 
+                             vm = self.obj_vm)
+        
+        ## Iterate through the sections 
+        while section.is_valid():
+            yield section 
+            section = section.NextSection.dereference_as("HPAK_SECTION") 
+
+class HPAKAddressSpace(standard.FileAddressSpace):
+    """ This AS supports the HPAK format """
+    
+    order = 30
+    
+    def __init__(self, base, config, **kwargs):
+        ## We must have an AS below us
+        self.as_assert(base, "No base Address Space")
+        standard.FileAddressSpace.__init__(self, base, config, layered = True, **kwargs)
+                
+        self.header = obj.Object("HPAK_HEADER", offset = 0, vm = base)
+                
+        ## Check the magic 
+        self.as_assert(self.header.Magic == 'HPAK', "Invalid magic found")
+                
+        self.physmem = None
+                
+        ## cycle though looking for the PHYSDUMP header
+        for section in self.header.Sections():
+            if str(section.Header) == "HPAKSECTHPAK_SECTION_PHYSDUMP":
+                self.physmem = section
+                break
+        
+        self.as_assert(self.physmem is not None, "Cannot find the PHYSDUMP section")
+                        
+    def read(self, addr, length):
+        return self.base.read(addr + self.physmem.Offset, length)
+        
+    def zread(self, addr, length):
+        return self.base.zread(addr + self.physmem.Offset, length)
+        
+    def is_valid_address(self, addr):
+        return self.base.is_valid_address(addr + self.physmem.Offset)
+        
+    def get_header(self):
+        return self.header
+        
+    def convert_to_raw(self, outfd):
+        """The standard imageinfo plugin won't work on 
+        hpak images so we provide this method. It wraps
+        the zlib compression if necessary"""
+        
+        d = zlib.decompressobj(16 + zlib.MAX_WBITS)
+        
+        chunk_size = 4096
+        chunks = self.physmem.Length / chunk_size
+        
+        def get_chunk(addr, size):
+            buffer = self.base.read(addr, size)
+            if self.physmem.Compressed == 1:
+                buffer = d.decompress(buffer)
+            return buffer
+        
+        for i in range(chunks):
+            outfd.write(get_chunk(self.physmem.Offset + i * chunk_size, chunk_size))
+            yield i 
+            
+        leftover = self.physmem.Length % chunk_size
+        
+        if leftover > 0:
+            outfd.write(get_chunk(self.physmem.Offset + i * chunk_size, leftover))
---
+++ volatility/plugins/hpakinfo.py
@@ -0,0 +1,51 @@
+# Volatility
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details. 
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+#
+
+import volatility.plugins.crashinfo as crashinfo
+import volatility.debug as debug
+
+class HPAKInfo(crashinfo.CrashInfo):
+    """Info on an HPAK file"""
+    
+    target_as = ['HPAKAddressSpace'] 
+    
+    def render_text(self, outfd, data):
+        
+        header = data.get_header()
+        
+        for section in header.Sections():
+            outfd.write("Header:     {0}\n".format(section.Header))
+            outfd.write("Length:     {0:#x}\n".format(section.Length))
+            outfd.write("Offset:     {0:#x}\n".format(section.Offset))
+            outfd.write("NextOffset: {0:#x}\n".format(section.NextSection))
+            outfd.write("Name:       {0}\n".format(section.Name))
+            outfd.write("Compressed: {0}\n".format(section.Compressed))
+            outfd.write("\n")
+            
+class HPAKExtract(HPAKInfo):
+    """Extract physical memory from an HPAK file"""
+    
+    def render_text(self, outfd, data):
+            
+        if not self._config.OUTPUT_FILE:
+            debug.error("You must supply --output-file")
+            
+        header = data.get_header()
+
+        data.convert_to_raw(outfd)
+        
+        print "Done."
