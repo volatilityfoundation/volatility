e9919996c742d34ee4f192b5a5a35979a01fbaf1
iMHLv2 <michael.ligh@mnin.org>
2013-01-29 16:13:05 +0000

    use table_header/table_row in mac_notifiers. use dereference_as(). just iterate arrays instead of using xrange and indexing

--- volatility/plugins/mac/notifiers.py
+++ volatility/plugins/mac/notifiers.py
@@ -32,33 +32,26 @@ class mac_notifiers(lsmod.mac_lsmod):
         common.set_plugin_members(self)
 
         (kernel_symbol_addresses, kmods) = common.get_kernel_addrs(self)
-
         gnotify_addr = common.get_cpp_sym("gNotifications", self.addr_space.profile)
-
-        gnotify_ptr  = obj.Object("Pointer", offset=gnotify_addr, vm=self.addr_space)
-
-        gnotifications = obj.Object("OSDictionary", offset=gnotify_ptr, vm=self.addr_space)
-
-        ents = obj.Object(theType = 'Array', offset = gnotifications.dictionary, vm = self.addr_space, targetType = 'dictEntry', count = gnotifications.count)
+        gnotify_ptr = obj.Object("Pointer", offset = gnotify_addr, vm = self.addr_space)
+        gnotifications = gnotify_ptr.dereference_as("OSDictionary")
+        ents = obj.Object('Array', offset = gnotifications.dictionary, vm = self.addr_space, targetType = 'dictEntry', count = gnotifications.count)
 
         # walk the current set of notifications
-        for i in xrange(0, gnotifications.count):
-            ent = ents[i]
+        for ent in ents:
 
             if ent == None:
                 continue
 
-            s = obj.Object("OSString", offset=ent.key, vm=self.addr_space)
-
-            key   = common.get_string(s.string.v(), self.addr_space)
+            s = ent.key.dereference_as("OSString") 
+            key = common.get_string(s.string.v(), self.addr_space)
 
             # get the value
-            valset   = obj.Object("OSOrderedSet", offset=ent.value, vm=self.addr_space)
+            valset = ent.value.dereference_as("OSOrderedSet")
+            notifiers_ptrs = obj.Object('Array', offset = valset.array, vm = self.addr_space, targetType = 'Pointer', count = valset.count)
             
-            notifiers_ptrs = obj.Object(theType = 'Array', offset = valset.array, vm = self.addr_space, targetType = 'Pointer', count = valset.count)
-            
-            for i in xrange(0, valset.count):
-                notifier = obj.Object("_IOServiceNotifier", offset=notifiers_ptrs[i], vm=self.addr_space)
+            for ptr in notifiers_ptrs:
+                notifier = ptr.dereference_as("_IOServiceNotifier") 
 
                 if notifier == None:
                     continue
@@ -70,7 +63,6 @@ class mac_notifiers(lsmod.mac_lsmod):
                 handler = notifier.handler
 
                 good = common.is_known_address(handler, kernel_symbol_addresses, kmods)
-
                 yield (good, key, notifier, matches)
 
     # returns the list of matching notifiers (serviceMatch) for a notifier as a string
@@ -78,31 +70,35 @@ class mac_notifiers(lsmod.mac_lsmod):
         matches = []
     
         matching = notifier.matching
+        ents = obj.Object('Array', offset = matching.dictionary, vm = self.addr_space, targetType = 'dictEntry', count = matching.count)
 
-        ents = obj.Object(theType = 'Array', offset = matching.dictionary, vm = self.addr_space, targetType = 'dictEntry', count = matching.count)
-
-        for i in xrange(0, matching.count):
-            ent = ents[i]
-
+        for ent in ents:
             if ent == None:
                 continue
 
-            s = obj.Object("OSString", offset=ent.key, vm=self.addr_space)
+            s = ent.key.dereference_as("OSString") 
+            key = common.get_string(s.string.v(), self.addr_space)
 
-            key   = common.get_string(s.string.v(), self.addr_space)
-
-            val = obj.Object("OSString", offset=ent.value, vm=self.addr_space)
-            match   = common.get_string(val.string.v(), self.addr_space, val.length)
+            val = ent.value.dereference_as("OSString")
+            match = common.get_string(val.string.v(), self.addr_space, val.length)
             
             matches.append(match)
 
         return ",".join(matches)
 
     def render_text(self, outfd, data):
+        self.table_header(outfd, [("Status", "10"), 
+                                  ("Key", "30"), 
+                                  ("Handler", "[addrpad]"), 
+                                  ("Matches", "")])
+
         for (good, key, notifier, matches) in data:
+
             if good == 0:
-                print "Unknown %s notifier handler: %x matches: %s" % (key, notifier.handler, matches)
+                status = "UNKNOWN"
             else:
-                print "Known %s notifier handler: %x matches: %s" % (key, notifier.handler, matches)
+                status = "OK"
+
+            self.table_row(outfd, status, key, notifier.handler, matches)
                 
 
