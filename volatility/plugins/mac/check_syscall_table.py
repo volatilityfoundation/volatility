87e9a1a56cf0b50d7430794f514960fbb93e593c
atcuno <andrew@memoryanalysis.net>
2013-02-19 18:18:48 +0000

    Mac - add check_syscalls

---
+++ volatility/plugins/mac/check_syscall_table.py
@@ -0,0 +1,55 @@
+# Volatility
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details. 
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+
+"""
+@author:       Andrew Case
+@license:      GNU General Public License 2.0 or later
+@contact:      atcuno@gmail.com
+@organization: 
+"""
+
+import volatility.obj as obj
+import common
+
+class mac_check_syscalls(common.AbstractMacCommand):
+    """ Checks to see if system call table entries are hooked """
+
+    def calculate(self):
+        common.set_plugin_members(self)
+
+        sym_addrs = self.profile.get_all_addresses()
+
+        nsysent = obj.Object("int", offset = self.get_profile_symbol("_nsysent"), vm = self.addr_space)
+        sysents = obj.Object(theType = "Array", offset = self.get_profile_symbol("_sysent"), vm = self.addr_space, count = nsysent, targetType = "sysent")
+
+        for (i, sysent) in enumerate(sysents):
+            ent_addr = sysent.sy_call.v()
+            hooked  = ent_addr not in sym_addrs
+
+            yield ("SyscallTable", i, ent_addr, hooked)
+ 
+    def render_text(self, outfd, data):
+        self.table_header(outfd, [("Table Name", "15"), ("Index", "6"), ("Address", "[addrpad]"), ("Symbol", "<30")])
+        for (table_name, i, call_addr, hooked) in data:
+            if hooked == False:
+                sym_name = self.profile.get_symbol_by_address("kernel", call_addr)
+            else:
+                sym_name = "HOOKED"
+
+            self.table_row(outfd, table_name, i, call_addr, sym_name)
+
+
+
