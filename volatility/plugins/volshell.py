c5f5bc902ca31c506669c21fab4c99a4f6f8b5e1
iMHLv2 <michael.ligh@mnin.org>
2013-04-22 16:46:32 +0000

    the -p/--pid option to volshell has never worked because its added as a comma-separated string, but we handle it as an int

--- volatility/plugins/volshell.py
+++ volatility/plugins/volshell.py
@@ -59,8 +59,8 @@ class volshell(common.AbstractWindowsCommand):
                           help = 'Operate on this Process name',
                           action = 'store', type = 'str')
         config.add_option('PID', short_option = 'p', default = None,
-                          help = 'Operate on these Process IDs (comma-separated)',
-                          action = 'store', type = 'str')
+                          help = 'Operate on this Process ID',
+                          action = 'store', type = 'int')
 
         self.addrspace = None
         self.proc = None
