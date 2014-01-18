cde7d1efa5df6389b0ed58c792fec988d91852c4
iMHLv2 <michael.ligh@mnin.org>
2014-01-17 20:11:11 -0600

    a few updates to linux_common
    https://github.com/iMHLv2/voldb/commit/a77f29ed052e9f30c5bafecb1bd2e5862
    dc99ae6

--- volatility/plugins/linux/common.py
+++ volatility/plugins/linux/common.py
@@ -29,6 +29,8 @@ import volatility.utils as utils
 import volatility.debug as debug
 import volatility.obj as obj
 
+from bisect import bisect_right
+
 MAX_STRING_LENGTH = 256
 
 nsecs_per = 1000000000
@@ -160,11 +162,84 @@ def do_get_path(rdentry, rmnt, dentry, vfsmnt):
 
     return ret_val
 
+def get_new_sock_pipe_path(dentry):
+    sym = dentry.obj_vm.profile.get_symbol_by_address("kernel", dentry.d_op.d_dname)
+    
+    if sym:
+        if sym == "sockfs_dname":
+            pre_name = "socket"    
+    
+        elif sym == "anon_inodefs_dname":
+            pre_name = "anon_inode"
+
+        elif sym == "pipefs_dname":
+            pre_name = "pipe"
+        else:
+            print "no handler for %s" % sym
+            pre_name = "<BAD>"
+
+        ret = "%s:[%d]" % (pre_name, dentry.d_inode.i_ino)
+
+    else:
+        ret = "<BAD d_dname pointer>"
+
+    return ret
+
 def get_path(task, filp):
     rdentry = task.fs.get_root_dentry()
     rmnt = task.fs.get_root_mnt()
     dentry = filp.dentry
     vfsmnt = filp.vfsmnt
 
-    return do_get_path(rdentry, rmnt, dentry, vfsmnt)
+    if dentry.d_op and dentry.d_op.d_dname:
+        ret = get_new_sock_pipe_path(filp.dentry)
+    else:
+        ret = do_get_path(rdentry, rmnt, dentry, vfsmnt)
 
+    return ret
+
+'''
+class LinuxStringsMethods:
+    def get_process_list(self):
+        import volatility.plugins.linux.pslist as linux_pslist
+
+        addr_space = utils.load_as(self._config)
+       
+        tasks = linux_pslist.linux_pslist.calculate(self)
+
+        try:
+            if self._config.PIDS is not None:
+                pidlist = [int(p) for p in self._config.PIDS.split(',')]
+                tasks = [t for t in tasks if int(t.pid) in pidlist]
+        except (ValueError, TypeError):
+            # TODO: We should probably print a non-fatal warning here
+            pass
+
+        return addr_space, tasks
+
+    def loaded_kernel_modules(self, addr_space):    
+        import volatility.plugins.linux.lsmod as linux_lsmod
+        
+        mods = dict((addr_space.address_mask(mod[0].module_core), mod[0]) for mod in linux_lsmod.linux_lsmod(self._config).calculate())
+        mod_addrs = sorted(mods.keys())
+         
+        return (mods, mod_addrs)
+
+    def find_module(self, modlist, mod_addrs, addr_space, addr):
+        pos = bisect_right(mod_addrs, addr) - 1
+        if pos == -1:
+            return None
+        mod = modlist[mod_addrs[pos]]
+
+        if (mod.obj_vm.address_compare(addr, mod.module_core) != -1 and
+                mod.obj_vm.address_compare(addr, mod.module_core + mod.core_size) == -1):
+            return mod
+        else:
+            return None
+
+    def get_module_name(self, module):
+        return module.m("name")
+
+    def get_task_pid(self, task):
+        return task.pid
+'''
