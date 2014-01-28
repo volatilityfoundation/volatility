7edb7d9bf03e921f55fae6a8b0cb06927a1ab125
iMHLv2 <michael.ligh@mnin.org>
2014-01-28 15:24:32 -0600

    initial commit of revised strings plugin to work with linux and mac

---
+++ volatility/plugins/linux/linux_strings.py
@@ -0,0 +1,111 @@
+# Volatility
+# Copyright (C) 2007,2008 Volatile Systems
+# Copyright (C) 2009 Timothy D. Morgan (strings optimization)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details. 
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+#
+
+from bisect import bisect_right
+import volatility.plugins.linux.pslist as linux_pslist
+import volatility.plugins.strings as strings
+import volatility.plugins.linux.common as linux_common
+import volatility.plugins.linux.lsmod as linux_lsmod
+
+class linux_strings(strings.Strings, linux_common.AbstractLinuxCommand):
+    """Match physical offsets to virtual addresses (may take a while, VERY verbose)"""
+
+    def __init__(self, config, *args, **kwargs):
+        strings.Strings.__init__(self, config, *args, **kwargs)
+
+    def get_processes(self, addr_space):
+        """Enumerate processes based on user options.
+
+        :param      addr_space | <addrspace.AbstractVirtualAddressSpace>
+
+        :returns    <list> 
+        """
+       
+        tasks = linux_pslist.linux_pslist(self._config).calculate()
+
+        try:
+            if self._config.PID is not None:
+                pidlist = [int(p) for p in self._config.PID.split(',')]
+                tasks = [t for t in tasks if int(t.pid) in pidlist]
+        except (ValueError, TypeError):
+            debug.error("Invalid PID {0}".format(self._config.PID))
+
+        return tasks
+
+    @classmethod
+    def get_modules(cls, addr_space):    
+        """Enumerate the kernel modules. 
+
+        :param      addr_space | <addrspace.AbstractVirtualAddressSpace>
+        
+        :returns    <tuple>
+        """
+
+        mask = addr_space.address_mask
+        modules = linux_lsmod.linux_lsmod(self._config).calculate()
+        mods = dict((mask(mod[0].module_core), mod[0]) for mod in modules)
+        mod_addrs = sorted(mods.keys())
+         
+        return (mods, mod_addrs)
+
+    @classmethod
+    def find_module(cls, mods, mod_addrs, addr_space, vpage):
+        """Determine which module owns a virtual page. 
+
+        :param      mods        | <list>
+                    mod_addrs   | <list>
+                    addr_space  | <addrspace.AbstractVirtualAddressSpace>
+                    vpage       | <int> 
+        
+        :returns    <module> || None
+        """
+
+        pos = bisect_right(mod_addrs, addr) - 1
+        if pos == -1:
+            return None
+        mod = modlist[mod_addrs[pos]]
+
+        compare = mod.obj_vm.address_compare
+        if (compare(addr, mod.module_core) != -1 and
+                compare(addr, mod.module_core + mod.core_size) == -1):
+            return mod
+        else:
+            return None
+
+    @classmethod
+    def get_module_name(cls, module):
+        """Get the name of a kernel module.
+
+        :param      module      | <module>
+        
+        :returns    <str>
+        """
+
+        return str(module.m("name"))
+
+    @classmethod
+    def get_task_pid(cls, task):
+        """Get the PID of a process. 
+
+        :param      task   | <task>
+        
+        :returns    <int>
+        """
+        return task.pid
+ 
--- volatility/plugins/strings.py
+++ volatility/plugins/strings.py
@@ -19,18 +19,19 @@
 #
 
 import os
-import volatility.plugins.taskmods as taskmods
-import volatility.plugins.filescan as filescan
-import volatility.obj as obj
-import volatility.utils as utils
 import volatility.win32 as win32
 import volatility.debug as debug
+import volatility.utils as utils
+import volatility.plugins.common as common
+import volatility.plugins.taskmods as taskmods
+import volatility.plugins.filescan as filescan
 
-class Strings(taskmods.DllList):
+class Strings(common.AbstractWindowsCommand):
     """Match physical offsets to virtual addresses (may take a while, VERY verbose)"""
+
     def __init__(self, config, *args, **kwargs):
-        taskmods.DllList.__init__(self, config, *args, **kwargs)
-        config.remove_option('PID')
+        common.AbstractWindowsCommand.__init__(self, config, *args, **kwargs)
+
         config.add_option('STRING-FILE', short_option = 's', default = None,
                           help = 'File output in strings format (offset:string)',
                           action = 'store', type = 'str')
@@ -39,72 +40,144 @@ class Strings(taskmods.DllList):
         config.add_option('OFFSET', short_option = 'o', default = None,
                           help = 'EPROCESS offset (in hex) in the physical address space',
                           action = 'store', type = 'int')
-        config.add_option('PIDS', short_option = 'p', default = None,
+        config.add_option('PID', short_option = 'p', default = None,
                           help = 'Operate on these Process IDs (comma-separated)',
-                          action = 'store', type = 'str')
+                          action = 'store', type = 'str')      
+  
+    def get_processes(self, addr_space):
+        """Enumerate processes based on user options.
 
-    def calculate(self):
-        """Calculates the physical to virtual address mapping"""
-        if self._config.STRING_FILE is None or not os.path.exists(self._config.STRING_FILE):
-            debug.error("Strings file not found")
+        :param      addr_space | <addrspace.AbstractVirtualAddressSpace>
 
-        addr_space = utils.load_as(self._config)
+        :returns    <list> 
+        """
 
+        bounce_back = taskmods.DllList.virtual_process_from_physical_offset
         if self._config.OFFSET != None:
-            tasks = [self.virtual_process_from_physical_offset(addr_space, self._config.OFFSET)]
+            tasks = [bounce_back(addr_space, self._config.OFFSET)]
         elif self._config.SCAN:
             procs = list(filescan.PSScan(self._config).calculate())
             tasks = []
             for task in procs:
-                tasks.append(self.virtual_process_from_physical_offset(addr_space, task.obj_offset))
+                tasks.append(bounce_back(addr_space, task.obj_offset))
         else:
             tasks = win32.tasks.pslist(addr_space)
 
         try:
-            if self._config.PIDS is not None:
-                pidlist = [int(p) for p in self._config.PIDS.split(',')]
+            if self._config.PID is not None:
+                pidlist = [int(p) for p in self._config.PID.split(',')]
                 tasks = [t for t in tasks if int(t.UniqueProcessId) in pidlist]
         except (ValueError, TypeError):
-            # TODO: We should probably print a non-fatal warning here
-            pass
+            debug.error("Invalid PID {0}".format(self._config.PID))
 
-        return addr_space, tasks
+        return tasks
 
-    def render_text(self, outfd, data):
-        """Runs through the text file outputting which string appears where"""
+    @classmethod
+    def get_modules(cls, addr_space):    
+        """Enumerate the kernel modules. 
 
-        addr_space, tasks = data
+        :param      addr_space | <addrspace.AbstractVirtualAddressSpace>
+        
+        :returns    <tuple>
+        """
+        
+        modules = win32.modules.lsmod(addr_space)
+        mask = addr_space.address_mask
+        mods = dict((mask(mod.DllBase), mod) for mod in modules)
+        mod_addrs = sorted(mods.keys())
+         
+        return (mods, mod_addrs)
+
+    @classmethod
+    def find_module(cls, mods, mod_addrs, addr_space, vpage):
+        """Determine which module owns a virtual page. 
+
+        :param      mods        | <list>
+                    mod_addrs   | <list>
+                    addr_space  | <addrspace.AbstractVirtualAddressSpace>
+                    vpage       | <int> 
+        
+        :returns    <_LDR_DATA_TABLE_ENTRY> || None
+        """
+        
+        mask = addr_space.address_mask
+        return win32.tasks.find_module(mods, mod_addrs, mask(vpage))
+
+    @classmethod
+    def get_module_name(cls, module):
+        """Get the name of a kernel module.
+
+        :param      module      | <_LDR_DATA_TABLE_ENTRY>
+        
+        :returns    <str>
+        """
+
+        return str(module.BaseDllName or '')
+
+    @classmethod
+    def get_task_pid(cls, task):
+        """Get the PID of a process. 
+
+        :param      task   | <_EPROCESS>
+        
+        :returns    <int>
+        """
+
+        return task.UniqueProcessId
+
+    def calculate(self):
+
+        if (self._config.STRING_FILE is None or 
+                    not os.path.exists(self._config.STRING_FILE)):
+            debug.error("Strings file not found")
+
+        addr_space = utils.load_as(self._config)
+        tasks = self.get_processes(addr_space)
 
         stringlist = open(self._config.STRING_FILE, "r")
+        reverse_map = self.get_reverse_map(addr_space, tasks)
 
-        verbfd = None
-        if self._config.VERBOSE:
-            verbfd = outfd
-
-        reverse_map = self.get_reverse_map(addr_space, tasks, verbfd)
-
-        for stringLine in stringlist:
-            (offsetString, string) = self.parse_line(stringLine)
+        for line in stringlist:
+            (offsetString, string) = self.parse_line(line)
             try:
                 offset = int(offsetString)
             except ValueError:
                 debug.error("String file format invalid.")
             if reverse_map.has_key(offset & 0xFFFFF000):
-                outfd.write("{0:08x} [".format(offset))
-                outfd.write(' '.join(["{0}:{1:08x}".format(pid[0], pid[1] | (offset & 0xFFF)) for pid in reverse_map[offset & 0xFFFFF000][1:]]))
-                outfd.write("] {0}\n".format(string.strip()))
+                yield "{0:08x} [".format(offset)
+                yield ' '.join(["{0}:{1:08x}".format(pid[0], pid[1] | (offset & 0xFFF)) for pid in reverse_map[offset & 0xFFFFF000][1:]])
+                yield "] {0}\n".format(string.strip())
 
-    @staticmethod
-    def get_reverse_map(addr_space, tasks, verbfd = None):
-        """Generates a reverse mapping from physical addresses to the kernel and/or tasks
+    @classmethod
+    def parse_line(cls, line):
+        """Parses a line of strings. 
+
+        :param      cls     | <Strings>
+                    line    | <str>
         
-           Returns:
-           dict of form phys_page -> [isKernel, (pid1, vaddr1), (pid2, vaddr2) ...]
-           where isKernel is True or False. if isKernel is true, list is of all kernel addresses
+        :returns    <tuple>
         """
+        # Remove any leading spaces to handle nasty strings output
+        stringLine = line.lstrip()
+        maxlen = len(line)
+        split_char = ' '
+        for char in [' ', ':']:
+            charpos = line.find(char)
+            if charpos < maxlen and charpos > 0:
+                split_char = char
+                maxlen = charpos
+        return tuple(line.split(split_char, 1))
 
-        if verbfd is None:
-            verbfd = obj.NoneObject("Swallow output unless VERBOSE mode is enabled")
+    @classmethod
+    def get_reverse_map(cls, addr_space, tasks):
+        """Generates a reverse mapping of physical addresses 
+        to the kernel and/or tasks.
+
+        :param      addr_space  | <addrspace.AbstractVirtualAddressSpace>
+                    tasks       | <list> 
+    
+        :returns    <dict>
+        """
 
         # ASSUMPTION: no pages mapped in kernel and userland
         # XXX: Can we eliminate the above assumption?  It seems like the only change needed for
@@ -117,41 +190,18 @@ class Strings(taskmods.DllList):
         #      recording vpage instead of vpage+i in the reverse map. -- TDM
         reverse_map = {}
 
-        verbfd.write("Enumerating kernel modules...\n")
-        mods = dict((addr_space.address_mask(mod.DllBase), mod) for mod in win32.modules.lsmod(addr_space))
-        mod_addrs = sorted(mods.keys())
-            
-        verbfd.write("Calculating kernel mapping...\n")
-        available_pages = addr_space.get_available_pages()
-        for (vpage, vpage_size) in available_pages:
-            kpage = addr_space.vtop(vpage)
-            for i in range(0, vpage_size, 0x1000):
-                # Since the output will always be mutable, we don't need to reinsert into the list
-                pagelist = reverse_map.get(kpage + i, None)
-                if pagelist is None:
-                    pagelist = [True]
-                    reverse_map[kpage + i] = pagelist
-                # Try to lookup the owning kernel module
-                module = win32.tasks.find_module(mods, mod_addrs, addr_space.address_mask(vpage + i))
-                if module:
-                    hint = str(module.BaseDllName)
-                else:
-                    hint = 'kernel'
-                pagelist.append((hint, vpage + i))
-                verbfd.write("\r  Kernel [{0:08x}]".format(vpage))
-        verbfd.write("\n")
-
-        verbfd.write("Calculating task mappings...\n")
+        debug.debug("Calculating task mappings...\n")
         for task in tasks:
             task_space = task.get_process_address_space()
-            verbfd.write("  Task {0} ...".format(task.UniqueProcessId))
-            process_id = int(task.UniqueProcessId)
+            debug.debug("  Task {0} ...".format(cls.get_task_pid(task)))
+            process_id = cls.get_task_pid(task)
             try:
                 available_pages = task_space.get_available_pages()
                 for (vpage, vpage_size) in available_pages:
                     physpage = task_space.vtop(vpage)
                     for i in range(0, vpage_size, 0x1000):
-                        # Since the output will always be mutable, we don't need to reinsert into the list
+                        # Since the output will always be mutable, we 
+                        # don't need to reinsert into the list
                         pagelist = reverse_map.get(physpage + i, None)
                         if pagelist is None:
                             pagelist = [False]
@@ -159,24 +209,41 @@ class Strings(taskmods.DllList):
                         if not pagelist[0]:
                             pagelist.append((process_id, vpage + i))
 
-                    verbfd.write("\r  Task {0} [{1:08x}]".format(process_id, vpage))
+                    debug.debug("\r  Task {0} [{1:08x}]".format(process_id, vpage))
             except (AttributeError, ValueError, TypeError):
                 # Handle most errors, but not all of them
                 continue
-            verbfd.write("\n")
-        verbfd.write("\n")
+            debug.debug("\n")
+        
+        debug.debug("\n")
+        debug.debug("Enumerating kernel modules...\n")
+        
+        (mods, mod_addrs) = cls.get_modules(addr_space)
+   
+        debug.debug("Calculating kernel mapping...\n")
+        available_pages = addr_space.get_available_pages()
+        for (vpage, vpage_size) in available_pages:
+            kpage = addr_space.vtop(vpage)
+            for i in range(0, vpage_size, 0x1000):
+                # Since the output will always be mutable, we 
+                # don't need to reinsert into the list
+                pagelist = reverse_map.get(kpage + i, None)
+                if pagelist is None:
+                    pagelist = [True]
+                    reverse_map[kpage + i] = pagelist
+                # Try to lookup the owning kernel module
+                module = cls.find_module(mods, mod_addrs, addr_space, vpage + i)
+                if module:
+                    hint = cls.get_module_name(module)
+                else:
+                    hint = 'kernel'
+                pagelist.append((hint, vpage + i))
+                debug.debug("\r  Kernel [{0:08x}]".format(vpage))
+        
+        debug.debug("\n")
         return reverse_map
 
-    @staticmethod
-    def parse_line(stringLine):
-        """Parses a line of strings"""
-        # Remove any leading spaces to handle nasty strings output
-        stringLine = stringLine.lstrip()
-        maxlen = len(stringLine)
-        split_char = ' '
-        for char in [' ', ':']:
-            charpos = stringLine.find(char)
-            if charpos < maxlen and charpos > 0:
-                split_char = char
-                maxlen = charpos
-        return tuple(stringLine.split(split_char, 1))
+    def render_text(self, outfd, data):
+
+        for result in data:
+            outfd.write(result)
