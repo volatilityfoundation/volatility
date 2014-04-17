521c9d50c87c1e5af11aafe0aeb48cc4db04adc1
gleeda <jamie@memoryanalysis.net>
2014-04-17 14:26:15 -0400

    modified so that profile is only forced if a plugin other than kdbgscan or imageinfo is selected

--- volatility/commands.py
+++ volatility/commands.py
@@ -88,8 +88,11 @@ class Command(object):
         """ Executes the plugin command."""
         # Check we can support the plugins
         profs = registry.get_plugin_classes(obj.Profile)
-        if self._config.PROFILE == None:
-            debug.error("You must set a profile!")
+        # force a profile if a plugin other than kdbgscan or imageinfo are given:
+        if self.__class__.__name__.lower() in ["kdbgscan", "imageinfo"] and self._config.PROFILE == None:
+            self._config.update("PROFILE", "WinXPSP2x86")
+        elif self._config.PROFILE == None:
+            debug.error("You must set a profile!" + self.args)
         if self._config.PROFILE not in profs:
             debug.error("Invalid profile " + self._config.PROFILE + " selected")
         if not self.is_valid_profile(profs[self._config.PROFILE]()):
