e8a6538f29fa844cc5d56b715ff0b9cd691dd32b
Andrew C <andrew@memoryanalysis.net>
2014-06-07 02:24:58 -0500

    Mac - allow module dump filtering by regex and base

--- volatility/plugins/mac/moddump.py
+++ volatility/plugins/mac/moddump.py
@@ -24,6 +24,7 @@
 @organization: 
 """
 import os
+import re
 import volatility.obj as obj
 import volatility.debug as debug
 import volatility.plugins.mac.common as common
@@ -34,17 +35,38 @@ class mac_moddump(common.AbstractMacCommand):
     def __init__(self, config, *args, **kwargs):         
         common.AbstractMacCommand.__init__(self, config, *args, **kwargs)
         self._config.add_option('BASE', short_option = 'b', default = None, help = 'Dump driver with BASE address (in hex)', action = 'store', type = 'int')
+        self._config.add_option('REGEX', short_option = 'r', help = 'Dump modules matching REGEX', action = 'store', type = 'string')
+        self._config.add_option('IGNORE-CASE', short_option = 'i', help = 'Ignore case in pattern match', action = 'store_true', default = False)
         self._config.add_option('DUMP-DIR', short_option = 'D', default = None, help = 'Output directory', action = 'store', type = 'str')
 
     def calculate(self):
         common.set_plugin_members(self)
 
-        module_address = int(self._config.BASE)
-     
-        # TODO -- enumerate module list and dump all if no filter 
-        kmod = obj.Object("kmod_info", offset = module_address, vm = self.addr_space)
+        if self._config.REGEX:
+            try:
+                if self._config.IGNORE_CASE:
+                    mod_re = re.compile(self._config.REGEX, re.I)
+                else:
+                    mod_re = re.compile(self._config.REGEX)
+            except re.error, e:
+                debug.error('Error parsing regular expression: {0}'.format(e))
+                
+        if self._config.BASE:
+            module_address = int(self._config.BASE)
+            yield obj.Object("kmod_info", offset = module_address, vm = self.addr_space)
+        else:
+            modules_addr = self.addr_space.profile.get_symbol("_kmod")
+            modules_ptr = obj.Object("Pointer", vm = self.addr_space, offset = modules_addr)
+            mod = modules_ptr.dereference_as("kmod_info")
 
-        yield kmod
+            while mod.is_valid():
+                if self._config.REGEX and not mod_re.search(str(mod.name)):
+                    mod = mod.next
+                    continue
+                
+                yield mod
+  
+                mod = mod.next
 
     def render_text(self, outfd, data):
         if (not self._config.DUMP_DIR or not os.path.isdir(self._config.DUMP_DIR)):
