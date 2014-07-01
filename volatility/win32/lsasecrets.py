8a81a6ab3b39a5d2418f5d8059e6a614f6b59e0c
gleeda <jamie@memoryanalysis.net>
2014-07-01 16:10:45 -0400

    lsasecrets: added a check before padding

--- volatility/win32/lsasecrets.py
+++ volatility/win32/lsasecrets.py
@@ -105,7 +105,8 @@ def decrypt_secret(secret, key):
         des_key = hashdump.str_to_key(block_key)
 
         des = DES.new(des_key, DES.MODE_ECB)
-        enc_block = enc_block + "\x00" * int(8 - len(enc_block))
+        if len(enc_block) < 8:
+            enc_block = enc_block + "\x00" * int(8 - len(enc_block))
         decrypted_data += des.decrypt(enc_block)
 
         j += 7
