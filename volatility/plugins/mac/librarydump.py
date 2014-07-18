cfe8b18aacc3d5a5c953cfa0670999df63710206
Andrew C <andrew@memoryanalysis.net>
2014-07-18 11:11:55 -0500

    Mac - update librarydump, procdump, and add load_diff

--- volatility/plugins/mac/librarydump.py
+++ volatility/plugins/mac/librarydump.py
@@ -28,6 +28,7 @@ import os
 import volatility.obj as obj
 import volatility.debug as debug
 import volatility.plugins.mac.pstasks as mac_tasks
+import volatility.plugins.mac.procdump as mac_procdump
 
 class mac_librarydump(mac_tasks.mac_tasks):
     """ Dumps the executable of a process """
@@ -37,36 +38,8 @@ class mac_librarydump(mac_tasks.mac_tasks):
         self._config.add_option('BASE', short_option = 'b', default = None, help = 'Dump driver with BASE address (in hex)', action = 'store', type = 'int')
         self._config.add_option('DUMP-DIR', short_option = 'D', default = None, help = 'Output directory', action = 'store', type = 'str')
 
-    def _get_executable_contents(self, proc, address):
-        text_map = None
-
-        proc_as = proc.get_process_address_space()
-
-        m = obj.Object("macho_header", offset = address, vm = proc_as)
-
-        buffer = ""
-        last_map = None
-
-        print "address: %x m: %s" % (address, m)
-
-        for seg in m.segments():
-            if str(seg.segname) == "__PAGEZERO":
-                continue
-
-            print "%s | %x" % (str(seg.segname), seg.vmaddr)
-            
-            if last_map:
-                pad_amt = map.start - last_map.end
-                pad = "\x00" * pad_amt
-            else:
-                pad = ""
-
-            buffer = buffer + pad + proc_as.zread(m.obj_offset + seg.vmaddr, seg.filesize)
- 
-        return buffer
- 
-    def _write_file(self, proc, exe_address):
-        exe_contents = self._get_executable_contents(proc, exe_address)
+    def _write_file(self, proc, exe_address, path):
+        exe_contents = mac_procdump.mac_procdump(self._config).get_executable_contents(proc, exe_address, path)
     
         file_name = "task.{0}.{1:#x}.dmp".format(proc.p_pid, exe_address)
         file_path = os.path.join(self._config.DUMP_DIR, file_name)
@@ -93,10 +66,10 @@ class mac_librarydump(mac_tasks.mac_tasks):
                 addresses = [self._config.BASE]
             else:
                 for map in proc.get_dyld_maps():        
-                    addresses.append(map.imageLoadAddress)
+                    addresses.append((map.imageFilePath, map.imageLoadAddress))
  
-            for address in addresses:
-                file_path = self._write_file(proc, address)
+            for (path, address) in addresses:
+                file_path = self._write_file(proc, address, path)
                 self.table_row(outfd, proc.p_comm, proc.p_pid, address, file_path)
 
 
