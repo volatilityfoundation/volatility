344fb8b564d668d91a3f268cd664d88ad99f7b80
iMHLv2 <michael.hale@gmail.com>
2014-07-28 19:51:07 -0500

    update pyinstaller specs and hooks for compiling on mac and linux

--- pyinstaller/hook-distorm3.py
+++ pyinstaller/hook-distorm3.py
@@ -9,4 +9,4 @@
 # if hasattr(sys, '_MEIPASS'):
 #     _distorm_path = sys._MEIPASS
 
-datas = [ ("C:\python27\Lib\site-packages\distorm3\distorm3.dll", ''), ]
+datas = [ ("C:\python27\Lib\site-packages\distorm3\distorm3.dll", ''), ('/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages/distorm3/libdistorm3.so', ""), ("/usr/local/lib/python2.7/dist-packages/distorm3/libdistorm3.so", "")]
