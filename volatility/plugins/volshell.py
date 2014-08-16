ff0b5b63f24c71193727fa29d67c24a234b58b09
iMHLv2 <michael.ligh@mnin.org>
2014-08-16 14:49:54 -0500

    refs #54 displaying types with dynamic attributes (i.e. callable offsets) is not supported in volshell

--- volatility/plugins/volshell.py
+++ volatility/plugins/volshell.py
@@ -374,57 +374,57 @@ class volshell(common.AbstractWindowsCommand):
             if address is not None:
                 objct = obj.Object(objct, address, space or self._proc.get_process_address_space())
 
-            if isinstance(objct, str):
-                try:
-                    size = profile.get_obj_size(objct)
-                    membs = [ (profile.get_obj_offset(objct, m), m, profile.vtypes[objct][1][m][1]) for m in profile.vtypes[objct][1] ]
-                    print "{0}".format("..." * depth), repr(objct), "({0} bytes)".format(size)
-                    for o, m, t in sorted(membs):
-                        print "{0}{1:6}: {2:30} {3}".format("..." * depth, hex(o), m, t)
-                        if recursive: 
-                            if t[0] in profile.vtypes:
-                                dt(t[0], recursive = recursive, depth = depth + 1)
-                except TypeError:
-                    print "Error: could not instantiate object"
+            try:
+                if isinstance(objct, str):
+                        size = profile.get_obj_size(objct)
+                        membs = [ (profile.get_obj_offset(objct, m), m, profile.vtypes[objct][1][m][1]) for m in profile.vtypes[objct][1] ]
+                        print "{0}".format("..." * depth), repr(objct), "({0} bytes)".format(size)
+                        for o, m, t in sorted(membs):
+                            print "{0}{1:6}: {2:30} {3}".format("..." * depth, hex(o), m, t)
+                            if recursive: 
+                                if t[0] in profile.vtypes:
+                                    dt(t[0], recursive = recursive, depth = depth + 1)
+                elif isinstance(objct, obj.BaseObject):
+                    membs = [ (o, m) for m, (o, _c) in objct.members.items() ]
+                    if not recursive:
+                        print repr(objct)
+                    offsets = []
+                    for o, m in sorted(membs):
+                        val = getattr(objct, m)
+                        if isinstance(val, list):
+                            val = [ str(v) for v in val ]
+
+                        # Handle a potentially callable offset
+                        if callable(o):
+                            o = o(objct) - objct.obj_offset
+
+                        offsets.append((o, m, val))
+
+                    # Deal with potentially out of order offsets
+                    offsets.sort(key = lambda x: x[0])
+
+                    for o, m, val in offsets:
+                        try:
+                            print "{0}{1:6}: {2:30} {3}".format("..." * depth, hex(o), m, val)
+                        except UnicodeDecodeError:
+                            print "{0}{1:6}: {2:30} -".format("..." * depth, hex(o), m)
+                        if recursive:
+                            if val.obj_type in profile.vtypes:
+                                dt(val, recursive = recursive, depth = depth + 1)
+                elif isinstance(objct, obj.NoneObject):
+                    print "ERROR: could not instantiate object"
                     print
-                    print "Reason: ", "object likely has dynamic attributes, try supplying an address"
-            elif isinstance(objct, obj.BaseObject):
-                membs = [ (o, m) for m, (o, _c) in objct.members.items() ]
-                if not recursive:
-                    print repr(objct)
-                offsets = []
-                for o, m in sorted(membs):
-                    val = getattr(objct, m)
-                    if isinstance(val, list):
-                        val = [ str(v) for v in val ]
-
-                    # Handle a potentially callable offset
-                    if callable(o):
-                        o = o(objct) - objct.obj_offset
-
-                    offsets.append((o, m, val))
-
-                # Deal with potentially out of order offsets
-                offsets.sort(key = lambda x: x[0])
-
-                for o, m, val in offsets:
-                    try:
-                        print "{0}{1:6}: {2:30} {3}".format("..." * depth, hex(o), m, val)
-                    except UnicodeDecodeError:
-                        print "{0}{1:6}: {2:30} -".format("..." * depth, hex(o), m)
-                    if recursive:
-                        if val.obj_type in profile.vtypes:
-                            dt(val, recursive = recursive, depth = depth + 1)
-            elif isinstance(objct, obj.NoneObject):
-                print "ERROR: could not instantiate object"
+                    print "Reason: ", objct.reason
+                else:
+                    print "ERROR: first argument not an object or known type"
+                    print
+                    print "Usage:"
+                    print
+                    hh(dt)
+            except TypeError:
+                print "Error: could not instantiate object"
                 print
-                print "Reason: ", objct.reason
-            else:
-                print "ERROR: first argument not an object or known type"
-                print
-                print "Usage:"
-                print
-                hh(dt)
+                print "Reason: ", "displaying types with dynamic attributes is currently not supported"
 
         def dis(address, length = 128, space = None, mode = None):
             """Disassemble code at a given address.
