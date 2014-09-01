3fd2bef9342684d9aa4f00b4a5c13d7f1d00a1a6
iquaba <cooka2011@gmail.com>
2014-08-31 22:49:46 -0500

    Adds ability to exclude settings from configuration files

--- volatility/plugins/saveconfig.py
+++ volatility/plugins/saveconfig.py
@@ -38,23 +38,40 @@ class SaveConfig(common.AbstractWindowsCommand):
     def __init__(self, config, *args, **kwargs):
         common.AbstractWindowsCommand.__init__(self, config, *args, **kwargs)
 
-        config.add_option("DEST", default = './volatilityrc',
-            cache_invalidator = False, help = "Destination of saved configuration file")
+        config.add_option("DEST", default = './volatilityrc', short_option = "D",
+            help = "Destination of saved configuration file")
 
+        config.add_option('EXCLUDE-CONF', default = False, short_option = "E",
+            action = "store_true", help = "Exclude settings from configuration files")
+
+        ## Used to make sure we don't save our own options
+        self._own_options = ["dest", "exclude_conf"]
 
     def calculate(self):
+        help( self._config)
         new_config = ConfigParser.RawConfigParser()
-        save_location = self._config.DEST
+        self.save_location = self._config.DEST
 
-        for key in self._config.cnf_opts:
-            yield key, self._config.cnf_opts[key]
-            new_config.set('DEFAULT', str(key), str(self._config.cnf_opts[key]))
+        ## Save options from configuration files
+        if self._config.EXCLUDE_CONF == False:
+            for key in self._config.cnf_opts:
+                yield key, self._config.cnf_opts[key]
+                new_config.set('DEFAULT', str(key), str(self._config.cnf_opts[key]))
 
-        with open(save_location, 'wb') as configfile:
+
+        ## Save current command line options
+        for key in self._config.opts:
+            if key not in self._own_options:
+                yield key, self._config.opts[key]
+                new_config.set('DEFAULT', str(key), str(self._config.opts[key]))
+
+        with open(self.save_location, 'wb') as configfile:
             new_config.write(configfile)
-            print("Saved command line options to " + save_location)
 
     def render_text(self, outfd, data):
+        outfd.write("\n")
+        self.table_header(outfd, [("Option", "20"), ("Value", "75")])
         for option in data:
-            outfd.write("{0}={1}\n".format(option[0],option[1]))
-
+            self.table_row(outfd, str(option[0]), str(option[1]))
+            #outfd.write("{0}\t=\t{1}\n".format(option[0], option[1]))
+        outfd.write("\nConfiguration saved to {}\n".format(self.save_location))
