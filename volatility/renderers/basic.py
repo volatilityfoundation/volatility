2d93c97d3828cf4845c3b69f4ae70d995107ba39
Mike Auty <mike.auty@gmail.com>
2014-10-05 23:06:31 +0100

    Clean up the basic renderers file.

--- volatility/renderers/basic.py
+++ volatility/renderers/basic.py
@@ -1,15 +1,11 @@
-import math
-from volatility.fmtspec import FormatSpec
-from volatility.renderers import ColumnSortKey
-
 __author__ = 'mike'
 
-from volatility import renderers
 
 class Address(long):
     """Integer class to allow renderers to differentiate between addresses and numbers"""
     def __new__(cls, number):
         return long.__new__(cls, number)
+
 
 class Address64(long):
     """Integer class to allow renderers to differentiate between addresses and numbers"""
@@ -23,3 +19,8 @@ class Hex(int):
 
     def __new__(cls, number):
         return int.__new__(cls, number)
+
+
+class Renderer(object):
+    def render(self, outfd, grid):
+        """Renders the content, ideally to outfd, but this is not strictly necessary"""
