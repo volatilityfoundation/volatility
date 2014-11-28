ae887c2e7e57f6ae0f260ac50d6863d12a2cadc2
gleeda <jamie@memoryanalysis.net>
2014-11-28 16:00:51 -0500

    converting linux_library_list

--- volatility/plugins/linux/library_list.py
+++ volatility/plugins/linux/library_list.py
@@ -25,6 +25,8 @@ import volatility.obj as obj
 import volatility.debug as debug
 import volatility.plugins.linux.common as linux_common
 import volatility.plugins.linux.pslist as linux_pslist
+from volatility.renderers import TreeGrid
+from volatility.renderers.basic import Address
 
 class linux_library_list(linux_pslist.linux_pslist):
     """ Lists libraries loaded into a process """
@@ -41,13 +43,14 @@ class linux_library_list(linux_pslist.linux_pslist):
 
                 yield task, mapping
 
-    def render_text(self, outfd, data):
-        self.table_header(outfd, [("Task", "16"),
-                          ("Pid", "8"),
-                          ("Load Address", "[addrpad]"),
-                          ("Path", ""),
-                          ])
-  
+    def unified_output(self, data):
+        return TreeGrid([("Task", str),
+                       ("Pid", int),
+                       ("LoadAddress", Address),
+                       ("Path", str)],
+                        self.generator(data))
+
+    def generator(self, data):
         for task, mapping in data:
-            self.table_row(outfd, task.comm, task.pid, mapping.l_addr, mapping.l_name)
+            yield (0, [str(task.comm), int(task.pid), Address(mapping.l_addr), str(mapping.l_name)])
 
