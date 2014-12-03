970b8049a40be17f2492598ec601bea37bfa83e1
Andrew C <andrew@memoryanalysis.net>
2014-12-02 22:04:40 -0600

    Linux - fix mount_cache

--- volatility/plugins/linux/mount_cache.py
+++ volatility/plugins/linux/mount_cache.py
@@ -23,6 +23,7 @@
 @organization: Digital Forensics Solutions
 """
 
+import volatility.obj as obj
 import volatility.plugins.linux.common as linux_common
 import volatility.plugins.linux.mount as linux_mount
 import volatility.plugins.linux.pslist as linux_pslist
@@ -38,8 +39,25 @@ class linux_mount_cache(linux_mount.linux_mount):
                         help = 'Show unallocated',
                         action = 'store_true')
 
+    def _get_filesystem_types(self):
+        all_fs = {}
+        
+        fs_ptr = obj.Object("Pointer", offset = self.addr_space.profile.get_symbol("file_systems"), vm = self.addr_space)
+        file_systems = fs_ptr.dereference_as("file_system_type")
+
+        fs = file_systems
+
+        while fs.is_valid():
+            fsname = obj.Object("String", offset = fs.name, vm = self.addr_space, length=256)
+            all_fs[str(fsname)] = fs
+            fs = fs.next
+
+        return all_fs
+
     def calculate(self):
         linux_common.set_plugin_members(self)
+        
+        fs_types = self._get_filesystem_types()
     
         # newer kernels
         if self.profile.has_type("mount"):
@@ -56,5 +74,11 @@ class linux_mount_cache(linux_mount.linux_mount):
             ns = None
 
         for mnt in cache:
-            yield (mnt, ns)
+            ret = self._parse_mnt(mnt, ns, fs_types)
+                    
+            if ret:
+                (mnt_sb, dev_name, path, fstype, rr, mnt_string) = ret
+
+                if not (dev_name == "devtmpfs" and path == "/"):
+                    yield (mnt_sb, dev_name, path, fstype, rr, mnt_string)
 
