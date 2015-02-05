760139e3b97993a8f2bc5dbab49273ad253a5ad2
Andrew C <andrew@memoryanalysis.net>
2015-02-05 10:11:57 -0600

    Linux - update enumerate_files to print inode address

--- volatility/plugins/linux/enumerate_files.py
+++ volatility/plugins/linux/enumerate_files.py
@@ -28,6 +28,7 @@ import volatility.obj as obj
 import volatility.plugins.linux.common as linux_common
 import volatility.plugins.linux.find_file  as linux_find_file
 from volatility.renderers import TreeGrid
+from volatility.renderers.basic import Address
 
 class linux_enumerate_files(linux_common.AbstractLinuxCommand):
     """Lists files referenced by the filesystem cache"""
@@ -35,14 +36,16 @@ class linux_enumerate_files(linux_common.AbstractLinuxCommand):
     def calculate(self):
         linux_common.set_plugin_members(self)
 
-        for (_, _, file_path, _)in linux_find_file.linux_find_file(self._config).walk_sbs():
-            yield file_path
+        for (_, _, file_path, file_dentry)in linux_find_file.linux_find_file(self._config).walk_sbs():
+            inode = file_dentry.d_inode
+
+            yield inode, inode.i_ino, file_path
 
     def unified_output(self, data):
-        return TreeGrid([("Path", str)],
+        return TreeGrid([("Inode Number", int), ("Inode", Address), ("Path", str)],
                         self.generator(data))
 
     def generator(self, data):
-        for path in data:
-            yield (0, [str(path)])
+        for inode, inum, path in data:
+            yield (0, [int(inum), Address(inode.v()), str(path)])
             
