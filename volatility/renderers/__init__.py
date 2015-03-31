956aa04f06e3e6da92448df788a4cb86e5945651
Mike Auty <mike@ikelos.org>
2015-03-31 21:53:22 +0100

    Optimize appending by removing the need to insert at a specific position.

--- volatility/renderers/__init__.py
+++ volatility/renderers/__init__.py
@@ -193,8 +193,14 @@ class TreeGrid(object):
 
     def _append(self, parent, values):
         """Adds a new node at the top level if parent is None, or under the parent node otherwise, after all other children."""
-        children = self.children(parent)
-        return self._insert(parent, len(children), values)
+        parent_path = ""
+        children = self._find_children(parent)
+        if parent is not None:
+            parent_path = parent.path + self.path_sep
+        newpath = parent_path + str(len(children))
+        tree_item = TreeNode(newpath, self, parent, values)
+        children.append((tree_item, []))
+        return tree_item
 
     def _insert(self, parent, position, values):
         """Inserts an element into the tree at a specific position"""
