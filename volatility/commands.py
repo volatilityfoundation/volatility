bdefa84340b83c38991c3ffebbe351382d1ba337
Sebastien Bourdon-Richard <sbourdon@SebHP.GICTQ.net>
2015-04-08 15:12:54 -0400

    QuickSqliteRenderer
    QuickSqliteRenderer for unified output

--- volatility/commands.py
+++ volatility/commands.py
@@ -29,7 +29,7 @@ import volatility.addrspace as addrspace
 from volatility.renderers.basic import Address, Address64, Hex, Bytes
 from volatility.renderers.dot import DotRenderer
 from volatility.renderers.html import HTMLRenderer, JSONRenderer
-from volatility.renderers.sqlite import SqliteRenderer
+from volatility.renderers.sqlite import SqliteRenderer, QuickSqliteRenderer
 from volatility.renderers.text import TextRenderer, FormatCellRenderer, QuickTextRenderer
 from volatility.renderers.xlsx import XLSXRenderer
 
@@ -282,7 +282,10 @@ class Command(object):
 
     def render_sqlite(self, outfd, data):
         self._render(outfd, SqliteRenderer(self.__class__.__name__, self._config), data)
-
+        
+    def render_quicksqlite(self, outfd, data):
+        self._render(outfd, QuickSqliteRenderer(self.__class__.__name__, self._config), data)
+		
     def render_dot(self, outfd, data):
         self._render(outfd, DotRenderer(self.text_cell_renderers, self._config), data)
 
