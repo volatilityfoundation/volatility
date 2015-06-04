aeaeda538a5b0423067f95ebecde9f3f4883c613
B Lebel <martarek@gmail.com>
2015-06-04 11:33:02 -0400

    Added unified outputs to some modules. Also, added "offset" to the output allowing easier manual exploration of the file. Finally added "addr_limit" output to threads as it can be used to detect processess that leverage exploits to expand the addr_limit in order to modify kernel objects (e.g. Towelroot on Android).

--- volatility/plugins/linux/kernel_opened_files.py
+++ volatility/plugins/linux/kernel_opened_files.py
@@ -22,12 +22,13 @@
 @author:       Andrew Case
 @license:      GNU General Public License 2.0
 @contact:      atcuno@gmail.com
-@organization: 
 """
 
 import volatility.obj as obj
 import volatility.plugins.linux.common as linux_common
 import volatility.plugins.linux.pslist as linux_pslist
+from volatility.renderers.basic import Address
+from volatility.renderers import TreeGrid
 
 class linux_kernel_opened_files(linux_common.AbstractLinuxCommand):
     """Lists files that are opened from within the kernel"""
@@ -35,7 +36,7 @@ class linux_kernel_opened_files(linux_common.AbstractLinuxCommand):
     def _walk_node_hash(self, node):
         last_node = None
         cnt = 0
-  
+
         hash_offset = self.addr_space.profile.get_obj_offset("dentry", "d_hash")
         while node.is_valid() and node != last_node:
             if cnt > 0:
@@ -45,11 +46,11 @@ class linux_kernel_opened_files(linux_common.AbstractLinuxCommand):
             cnt = cnt + 1
             last_node = node
             node = dentry.d_hash.next
-    
+
     def _walk_node_node(self, node):
         last_node = None
         cnt = 0
-  
+
         while node.is_valid() and node != last_node:
             if cnt > 0:
                 yield node, cnt
@@ -57,12 +58,12 @@ class linux_kernel_opened_files(linux_common.AbstractLinuxCommand):
             cnt = cnt + 1
             last_node = node
             node = node.next
-            
-    def _walk_node(self, node): 
+
+    def _walk_node(self, node):
         last_node = None
 
         yield node, 0
-        
+
         for node, cnt in self._walk_node_node(node):
             yield node, cnt
 
@@ -71,26 +72,26 @@ class linux_kernel_opened_files(linux_common.AbstractLinuxCommand):
 
     def _gather_dcache(self):
         d_hash_shift = obj.Object("unsigned int", offset =self.addr_space.profile.get_symbol("d_hash_shift"), vm = self.addr_space)
-        loop_max = 1 << d_hash_shift 
+        loop_max = 1 << d_hash_shift
 
         d_htable_ptr = obj.Object("Pointer", offset = self.addr_space.profile.get_symbol("dentry_hashtable"), vm = self.addr_space)
 
         arr = obj.Object(theType = "Array", targetType = "hlist_bl_head", offset = d_htable_ptr, vm = self.addr_space, count = loop_max)
 
         hash_offset = self.addr_space.profile.get_obj_offset("dentry", "d_hash")
-       
+
         dents = {}
 
         for list_head in arr:
             if not list_head.first.is_valid():
                 continue
 
-  
+
             node = obj.Object("hlist_bl_node", offset = list_head.first & ~1, vm = self.addr_space)
-            
-            for node, cnt in self._walk_node(node):   
+
+            for node, cnt in self._walk_node(node):
                 dents[node.v() - hash_offset] = 0
-    
+
         return dents
 
     def _compare_filps(self):
@@ -115,9 +116,18 @@ class linux_kernel_opened_files(linux_common.AbstractLinuxCommand):
         linux_common.set_plugin_members(self)
 
         for dentry_offset in self._compare_filps():
-            dentry = obj.Object("dentry", offset = dentry_offset, vm = self.addr_space) 
+            dentry = obj.Object("dentry", offset = dentry_offset, vm = self.addr_space)
             if dentry.d_count > 0 and dentry.d_inode.is_reg() and dentry.d_flags == 128:
                 yield dentry
+
+    def generator(self,data):
+        for dentry in data:
+            yield(0,Address(dentry.obj_offset),str(dentry.get_partial_path()))
+
+    def unified_output(self, data):
+        return TreeGrid([("Offset (V)",Address),
+                        ("Partial File Path",str)],
+                        self.generator(data))
 
     def render_text(self, outfd, data):
 
