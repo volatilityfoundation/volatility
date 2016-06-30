7b18b516ab91fd164429e642791c6bc912d3e9e9
atcuno <andrew@dfir.org>
2016-06-30 13:31:46 -0500

    Windows - update drivermodule with more information

--- volatility/plugins/drivermodule.py
+++ volatility/plugins/drivermodule.py
@@ -65,31 +65,35 @@ class drivermodule(common.AbstractWindowsCommand):
 
         else:                
             for driver in drivers:
-                driver_name = driver.get_object_header().NameInfo.Name
+                driver_name  = str(driver.get_object_header().NameInfo.Name or '')
+                service_key = str(driver.DriverExtension.ServiceKeyName or '')
+                driver_name3 = str(driver.DriverName or '')
+                
                 owning_module = tasks.find_module(mods, mod_addrs, mods.values()[0].obj_vm.address_mask(driver.DriverStart))
-
                 if owning_module:
                     module_name = owning_module.BaseDllName or owning_module.FullDllName
                 else:
                     module_name = "UNKNOWN"
 
-                yield (module_name, driver_name)
+                yield (module_name, driver_name, service_key, driver_name3)
 
     def generator(self, data):
 
-        for module_name, driver_name in data:
-            yield( 0, [str(module_name), str(driver_name)])
+        for module_name, driver_name, service_key, driver_name3 in data:
+            yield( 0, [str(module_name), str(driver_name), str(service_key), str(driver_name3)])
 
     def unified_output(self, data):
         return TreeGrid([("Module", str),
-                         ("Driver", str)],
+                         ("Driver", str),
+                         ("Alt. Name", str),
+                         ("Service Key", str)],
                         self.generator(data))
 
     def render_text(self, outfd, data):
-        self.table_header(outfd, [("Module", "36"), ("Driver", "")])
+        self.table_header(outfd, [("Module", "36"), ("Driver", "24"), ("Alt. Name", "24"), ("Service Key", "")])
 
-        for module_name, driver_name in data:
-            self.table_row(outfd, module_name, driver_name)
+        for module_name, driver_name, service_key, driver_name3 in data:
+            self.table_row(outfd, module_name, driver_name, service_key, driver_name3)
        
 
 
