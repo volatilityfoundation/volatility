volatilityfoundation/volatility

commit 16af7d3d7db818d53fcbcc93e3620dcfbc063a09

Linux - update lsmod for newer kernels

volatility/plugins/overlays/linux/linux.py
@@ -820,6 +820,29 @@ class net_device(obj.CType):
         return self.flags & 0x100 == 0x100 # IFF_PROMISC
 
 class module_struct(obj.CType):
+    
+    @property
+    def init_size(self):
+        if hasattr(self, "init_layout"):
+            ret = self.m("init_layout").m("size")
+        elif hasattr(self, "init_size"):
+            ret = self.m("init_size")
+        else:
+            ret = 0
+
+        return ret
+ 
+    @property 
+    def core_size(self):
+        if hasattr(self, "core_layout"):
+            ret = self.m("core_layout").m("size")
+        elif hasattr(self, "core_size"):
+            ret = self.m("core_size")
+        else:
+            ret = 0
+
+        return ret
+        
     def _get_sect_count(self, grp):
         arr = obj.Object(theType = 'Array', offset = grp.attrs, vm = self.obj_vm, targetType = 'Pointer', count = 25)
 

commit 69bb1621710154a07ad60fbfc2bc881a9872b43a

We fixed the infinite calling recursion in core_size and init_size properties. This bug was introduced in commit 16af7d3d7db818d53fcbcc93e3620dcfbc063a09

volatility/plugins/overlays/linux/linux.py
@@ -842,10 +842,8 @@ class module_struct(obj.CType):
     def init_size(self):
         if hasattr(self, "init_layout"):
             ret = self.m("init_layout").m("size")
-        elif hasattr(self, "init_size"):
-            ret = self.m("init_size")
         else:
-            ret = 0
+            ret = self.m("init_size")
 
         return ret
  
@@ -853,10 +851,8 @@ class module_struct(obj.CType):
     def core_size(self):
         if hasattr(self, "core_layout"):
             ret = self.m("core_layout").m("size")
-        elif hasattr(self, "core_size"):
-            ret = self.m("core_size")
         else:
-            ret = 0
+            ret = self.m("core_size")
 
         return ret
         