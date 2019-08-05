fca48ed854cb12e9ae64597bf19c0c8d5514e597
MalwareMechanic <malwaremechanic@gmail.com>
2019-08-05 14:30:06 -0400

    Updated license information

--- volatility/plugins/win10deflate.py
+++ volatility/plugins/win10deflate.py
@@ -1,24 +1,21 @@
-# Volatility
-# Copyright (C) 2007-2019 Volatility Foundation
+# Copyright (C) 2019 FireEye, Inc.
 #
-# Authors:
-# Blaine Stancill <blaine.stancill@FireEye.com>
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
 #
-# This file is part of Volatility.
-#
-# Volatility is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# Volatility is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Volatility.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
+# Authors:
+# Blaine Stancill <blaine.stancill@FireEye.com>
 
 import volatility.debug as debug
 import volatility.plugins.common as common
